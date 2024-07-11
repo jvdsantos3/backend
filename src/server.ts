ecf0f97bd5f67ddf82e235df94c3603e36342946
joao <jv.dossantos012@gmail.com>
2024-07-11 12:34:06 -0300

    create and get activities

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import cors from "@fastify/cors";
 import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
 
 const app = fastify()
 
@@ -15,6 +18,9 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
 app.register(confirmTrip)
+app.register(confirmParticipant)
+app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!')
