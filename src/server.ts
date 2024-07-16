c3fe3780c71955d86edacb0dc936785eeebcd7ea
joao <jv.dossantos012@gmail.com>
2024-07-16 20:04:37 -0300

    invites and participants

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,10 @@ import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
+import { updateTrip } from "./routes/update-trip";
+import { getTripDetails } from "./routes/get-trip-details";
 
 const app = fastify()
 
@@ -25,6 +29,10 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!')
